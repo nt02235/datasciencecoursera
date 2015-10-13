aea8e90ce32f9c2c8c9714485ba94aae127dbb15
nt02235 <nathalie.torres@gmail.com>
2015-10-13 00:13:27 -0400

    rpog-033 week 2 assignment part 2 completion

---
+++ rprog033/2-week/complete.R
@@ -0,0 +1,20 @@
+complete <- function(directory, id = 1:332) {
+   record <- data.frame(id = integer(0), nobs = integer(0))
+   
+   files <- vector('character')
+   files <- paste("00", id[id < 10], sep ="")
+   files <- c(files, id[id > 99])
+   files <- c(files, paste("0", id[id > 9 & id < 100], sep = ""))
+   
+   if(sum(nchar(files) < 3) != 0)
+      files <- files[-which(nchar(files) != 3)]
+   files <- paste(directory,"/", files,".csv",sep="")
+   
+   for(i in 1:length(id)) {
+      data <- read.csv(files[i], header = T)
+      record[i,"id"] <- id[i]
+      record[i,"nobs"] <- nrow(data[!is.na(data$sulfate) & !is.na(data$nitrate),])
+   }
+   
+   record
+}
