be064e267891862d8634f6188f60922915019a9e
nt02235 <nathalie.torres@gmail.com>
2015-10-17 13:25:12 -0400

    Fixed complete.R and corr.R so that they passed test scripts. Specifically adding prefixes to id more efficiently

--- rprog033/2-week/complete.R
+++ rprog033/2-week/complete.R
@@ -2,13 +2,13 @@ complete <- function(directory, id = 1:332) {
    record <- data.frame(id = integer(0), nobs = integer(0))
    
    files <- vector('character')
-   files <- paste("00", id[id < 10], sep ="")
-   files <- c(files, paste("0", id[id > 9 & id < 100], sep = ""))
-   files <- c(files, id[id > 99])
-   
-   if(sum(nchar(files) < 3) != 0)
-      files <- files[-which(nchar(files) != 3)]
-   files <- paste(directory,"/", files,".csv",sep="")
+   files <- paste(directory, "/", sprintf("%03d", id), ".csv", sep = "")
+#    files <- paste(directory, "/", files,".csv",sep="")
+#    files <- paste("00", id[id < 10], sep ="")
+#    files <- c(files, paste("0", id[id > 9 & id < 100], sep = ""))
+#    files <- c(files, id[id > 99])   
+#    if(sum(nchar(files) < 3) != 0)
+#       files <- files[-which(nchar(files) != 3)]
    
    for(i in 1:length(id)) {
       data <- read.csv(files[i], header = T)
--- rprog033/2-week/corr.R
+++ rprog033/2-week/corr.R
@@ -1,16 +1,13 @@
 corr <- function(directory, threshold = 0) {
    records <- complete(directory)
-   records <- record[which(records$nobs > threshold), -2]
+   records <- records[which(records$nobs > threshold), -2]
    
    cor <- numeric()
    
    if(length(records) != 0){
       
       files <- vector('character')
-      files <- paste("00", records[records < 10], sep ="")
-      files <- c(files, paste("0", records[records > 9 & records < 100], sep = ""))
-      files <- c(files, records[records > 99])
-      files <- paste(directory,"/", files,".csv",sep="")
+      files <- paste(directory, "/", sprintf("%03d", records), ".csv", sep = "")
       
       for(i in 1:length(records)){
          data <- read.csv(files[i], header = T)
